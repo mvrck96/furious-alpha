ef0ee84579fa291ce19fb046be47d653086ee3c5
mvrck96 <maverickmsk1@gmail.com>
2022-06-26 22:42:47 +0300

    Added simple get request to yahoo finance api

--- src/serv/main.py
+++ src/serv/main.py
@@ -1,9 +1,23 @@
+from os import getenv
+
+import requests
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
 
+load_dotenv()
 app = FastAPI()
 
+YAHOO_URL = "https://yfapi.net/v8/finance/spark"
+YAHOO_TOKEN = getenv("YAHOO_TOKEN")
 
-@app.get("/")
-def test() -> None:
-    return "Test!"
+
+@app.get("/history")
+def test(ticker: str = None):
+    querystring = {"symbols": ticker}
+    headers = {"x-api-key": YAHOO_TOKEN}
+    res = requests.request(
+        "GET", YAHOO_URL, headers=headers, params=querystring
+    )
+    return res.text
